fa27667703f82939cb9817e8b9bfb48db8d18b89
Ashutosh Krishna Das <ashutosh@harekrishna.ru>
2018-04-02 16:45:29 +0300

    Implement double_consonants. Now it does the forward replacement (karma => karmma), but not the reverse replacement of extra doubles (brahmmA => brahmA).

--- double_consonants.js
+++ double_consonants.js
@@ -1,11 +1,25 @@
 function double_consonants(string) {
-    var vwl = /(?:a|A|i|I|u|U|~r|~R|~l|~L|e|ai|o|au)/;
+    var vowel = /(?:a|A|i|I|u|U|~r|~R|~l|~L|e|ai|o|au)/;
     var cns = /(?:k|kh|g|gh|~N|ch|chh|j|jh|~n|T|Th|D|Dh|N|t|th|d|dh|n|p|ph|b|bh|m|y|r|l|v|sh|Sh|s|h|L)/;
-    var cns_no_hbbhv = /(?:k|kh|g|gh|~N|ch|chh|j|jh|~n|T|Th|D|Dh|N|t|th|d|dh|n|p|ph|m|y|r|l|sh|Sh|s|L)/;
-    var cns_dbl = /(?:kk|kkh|gg|ggh|~N~N|chch|chchh|jj|jjh|~n~n|TT|TTh|DD|DDg|NN|tt|tth|dd|ddh|nn|pp|pph|mm|yy|rr|ll|shsh|ShSh|ss|LL)/;
+    var consonant_no_hbbhv = /(?:k|kh|g|gh|~N|ch|chh|j|jh|~n|T|Th|D|Dh|N|t|th|d|dh|n|p|ph|m|y|r|l|sh|Sh|s|L)/;
+    var cns_dbl = /(?:kk|kkh|gg|ggh|~N~N|chch|chchh|jj|jjh|~n~n|TT|TTh|DD|DDh|NN|tt|tth|dd|ddh|nn|pp|pph|mm|yy|rr|ll|shsh|ShSh|ss|LL)/;
 
     var mahaaprana_pairs = /(?:kh|gh|chh|jh|Th|Dh|th|dh|ph)/;
-    return 'karmma';
+    regexp = new RegExp(
+        '('
+            +vowel.source
+            +'r'
+        +')'
+        +'(?!'+cns_dbl.source+')'
+        +'(?!(?:sh|Sh|s)' + vowel.source + ')'
+
+        +'('
+        +consonant_no_hbbhv.source
+        +')'
+    );
+    newstr = '$1$2$2';
+
+    return string.replace(regexp, newstr);
 }
 
 module.exports = double_consonants;
--- test/double_consonants.test.js
+++ test/double_consonants.test.js
@@ -2,7 +2,33 @@ const assert = require('assert');
 const dbl = require('../double_consonants');
 
 describe('double_consonants', function() {
-    it('should turn karma into karmma', function() {
-        assert.equal(dbl('karma'), 'karmma');
+    var pairs = [
+        ["arkaH", "arkkaH"],
+        ["svargaH", "svarggaH"],
+        ["pArthaH", "pArtthaH"],
+        ["karma", "karmma"],
+        ["sUryaH", "sUryyaH"],
+        ["durlabhaH", "durllabhaH"],
+        ["brahmA", "brahmA"],
+        ["sudarshanaH", "sudarshanaH"],
+        ["karShati", "karShati"],
+        ["sarvam", "sarvam"],
+        ["arbudam", "arbudam"],
+        ["garbhaH", "garbhaH"],
+        ["brahmA", "brahmA"],
+        ["vahniH", "vahniH"],
+        ["jAhnavI", "jAhnavI"],
+    ];
+    pairs.forEach(function(pair) {
+        var a = pair[0];
+        var b = pair[1];
+        it(a + ' => ' + b, function() {
+            assert.equal(b, dbl(a));
+        });
+        if (a !== b) {
+            it(a + ' => ' + b, function() {
+                assert.equal(b, dbl(b));
+            });
+        }
     });
 });
